d7586e0528e9d7ad92e133da939705eca469b5df
Nasubikun <j08012333439@gmail.com>
2021-11-08 00:30:59 +0900

    version 0.0.2

--- README.md
+++ README.md
@@ -2,11 +2,29 @@
 
 While editing a JSX or TSX file, you can open the corresponding css module file.
 
-![openCssModuleDemo](openCssModuleDemo.gif)
+![openCssModuleDemo](imgs/openCssModuleDemo.gif)
 
 ## Usage
 
 After install this extension, just open the command palette (<kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>P</kbd>) and choose `open css module` or `open css module (Beside)`.
+
+## Setting
+
+Open setting (<kbd>Ctrl</kbd> + <kbd>,</kbd>) and open `OpenCssModule` tab.
+You can enable **Auto Create** function.
+
+![autoCreate](imgs/autoCreate.png)
+
+Auto Create allows create new files when there are no css modules.
+Set the **Styles Directory** as the directory where the files should be created.
+
+## Updates
+
+### 0.0.2
+
+- add autoCreate method
+- refactor code
+
 
 ### 0.0.1
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+// import * as fs from 'fs'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,45 +13,111 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
+
+    // import sentence and css module file exist
+    // import sentence exists but css module file does not exist
+    // import sentence and css module file do not exist
     let disposable = vscode.commands.registerCommand('open-css-module.openCssModule', () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
-        const cssModulePath = getCssModulePath();
-        if(cssModulePath === null) return;
-        vscode.window.showInformationMessage(`opening... ${cssModulePath}`);
-        openFileInVscode(cssModulePath)
+        const beside = false;
+        handleCssModuleOpen(beside);
     });
 
     let disposableBeside = vscode.commands.registerCommand('open-css-module.openCssModuleBeside', () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
-        const cssModulePath = getCssModulePath();
-        if(cssModulePath === null) return;
-        vscode.window.showInformationMessage(`opening... ${cssModulePath}`);
-        openFileInVscodeBeside(cssModulePath)
+        const beside = true;
+        handleCssModuleOpen(beside);
     });
-
     context.subscriptions.push(disposable);
 }
 
-function openFileInVscode(filePath: string) {
-    const relativePath = '../' + filePath
-    const currentlyOpenTabfileUri = getActiveEditor()?.document.uri;
-    if(currentlyOpenTabfileUri === undefined) return;
-    const openPath = vscode.Uri.joinPath(currentlyOpenTabfileUri,relativePath)
-    vscode.workspace.openTextDocument(openPath).then(doc => {
-        vscode.window.showTextDocument(doc);
-    });
+
+function handleCssModuleOpen(beside: boolean){
+    const config = vscode.workspace.getConfiguration('openCssModule')
+    var cssModulePath = getCssModulePath();
+    // const importSentenceExists = cssModulePath?true:false;
+    const cssModuleFileExists = checkCssModuleFileExists(cssModulePath);
+    vscode.window.showInformationMessage(`opening... ${cssModulePath}`);
+    vscode.window.showInformationMessage(`${config}`);
+    if (cssModulePath && !cssModuleFileExists){
+        createAndOpenCssModuleWithImport(cssModulePath, beside)
+    }else if(!cssModulePath && !cssModuleFileExists){
+        createAndOpenCssModuleWithoutImport(config.stylesDirectory, beside)
+    }else if(cssModulePath) openFileInVscode(cssModulePath,beside);
 }
 
-function openFileInVscodeBeside(filePath: string) {
-    const relativePath = '../' + filePath
-    const currentlyOpenTabfileUri = getActiveEditor()?.document.uri;
-    if(currentlyOpenTabfileUri === undefined) return;
-    const openPath = vscode.Uri.joinPath(currentlyOpenTabfileUri,relativePath)
-    vscode.workspace.openTextDocument(openPath).then(doc => {
-        vscode.window.showTextDocument(doc,{viewColumn: vscode.ViewColumn.Beside});
+
+function createAndOpenCssModuleWithoutImport(stylesDirectory: string, beside: boolean){
+    const path = require('path');
+    const relativePath = path.join(stylesDirectory,getModuleName())+'.module.css'
+    const cssModuleFilePath = relativePath2AbsolutePath(relativePath);
+    insertToTop(generateImportSentence(relativePath))
+    createAndOpenFile(cssModuleFilePath, beside);
+    return cssModuleFilePath
+}
+
+
+function createAndOpenCssModuleWithImport(importFilePath:string, beside: boolean){
+    return createAndOpenFile(importFilePath, beside);
+}
+
+
+function createAndOpenFile(filePath: string, beside: boolean){
+    const wsedit = new vscode.WorkspaceEdit()
+    const fileUri = vscode.Uri.file(filePath)
+    wsedit.createFile(fileUri, { ignoreIfExists: false });
+    vscode.workspace.applyEdit(wsedit).then(val =>{
+        openFileInVscode(filePath,beside)
     });
+    return filePath;
+}
+
+
+function openFileInVscode(filePath: string, beside: boolean) {
+    // open css module file and return if it did
+    
+    const openPath = vscode.Uri.file(filePath)
+
+    if (beside){
+        vscode.workspace.openTextDocument(openPath).then(doc => {
+            vscode.window.showTextDocument(doc,{viewColumn: vscode.ViewColumn.Beside});
+        });
+    }
+        vscode.workspace.openTextDocument(openPath).then(doc => {
+            vscode.window.showTextDocument(doc);
+    });
+    return true
+}
+
+
+function relativePath2AbsolutePath(filePath: string): string{
+    const path = require('path');
+    const relativePath = '../' + filePath
+    const currentlyOpenTabfilePath = getActiveEditor()?.document.uri?.fsPath;
+    return path.join(currentlyOpenTabfilePath,relativePath)
+}
+
+
+function checkCssModuleFileExists(filePath: string|null){
+    if(filePath === null) return false;
+    const fs = require('fs');
+    return fs.existsSync(filePath);
+}
+
+
+function insertToTop(sentence: string){
+    const activeTextEditor = getActiveEditor()
+    let f = function(editBuilder: vscode.TextEditorEdit): void{
+        editBuilder.insert(new vscode.Position(0,0), sentence);
+    }
+    activeTextEditor?.edit(f)
+}
+
+
+function generateImportSentence(importPath: string) :string{
+    return `import styles from "${importPath.replace(/\\/g, '/')}"\n`
 }
 
 
@@ -61,6 +128,25 @@ function getActiveEditor(): vscode.TextEditor | null {
         return null;
     }
     return editor;
+}
+
+
+function getModuleName(): string|null {
+    // アクティブエディタの取得 =========================================================
+    const editor = getActiveEditor();
+    if (editor === null) return null;
+
+    // エディタ内の情報取得 ============================================================
+    const doc = editor.document;
+    const filePath = doc.fileName;
+    const path = require('path');
+    // const fileNameArr = filePath.split('/')
+    // const fileName = fileNameArr[fileNameArr.length - 1]
+    const moduleName = path.parse(filePath).name
+
+
+    return moduleName
+
 }
 
 
@@ -80,7 +166,7 @@ function getCssModulePath(): string | null {
     }
     const importStatement = res[0];
     vscode.window.showInformationMessage(`found: ${importStatement}`);
-    return res[1]
+    return relativePath2AbsolutePath(res[1])
 }
 // this method is called when your extension is deactivated
 export function deactivate() {}
