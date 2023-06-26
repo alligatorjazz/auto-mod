7dcb3fb33220c90e08c9bdfa20bf5935c69b56bb
Damian Thomas <dimitrisonic@gmail.com>
2023-06-26 07:36:08 -0400

    0.1.0 ready

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -9,11 +9,30 @@ import * as fs from "fs";
 suite("Extension Test Suite", () => {
 	vscode.window.showInformationMessage("Start all tests.");
 
-	const langs = [".css", ".scss", ".sass", "less"];
-	// wipes temp folder
-	fs.rmSync("./*", { force: true, recursive: true });
+	const styleLangs = [".css", ".scss", ".sass", ".less"];
+	const tsxContent = "export function Test() {\nreturn <div></div>\n}";
 
-	test("Creating style module here", () => {
+	const wipeTestFolder = () => fs.rmSync("./*", { recursive: true });
+	const createTsxFile = () => fs.writeFileSync("./Test.tsx", tsxContent, { flag: "w" });
+	
+	console.log("Working directory: ", process.cwd());
+	console.log(fs.readdirSync("."));
 
+	test("Creating style module here", async () => {
+		
+		const results = await Promise.all(styleLangs.map(async lang => {
+			wipeTestFolder();
+			createTsxFile();
+
+			
+			await vscode.commands.executeCommand("auto-mod.createStyleModuleHere");
+			return fs.existsSync(`./Test.module${lang}`);
+		}));
+
+		if (!results.includes(false)) {
+			return true;
+		}
+
+		return false;
 	});
 });
